71e4c6fe20d939eb717bcdc4fc5c1e4f84c4a417
chippypsasikumar@gmail.com <chippypsasikumar@gmail.com>
2024-05-24 16:15:40 +0530

    Add project files.

---
+++ WCFServicePrjectAsp/App_Code/Service.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class Service : IService
+{
+	connectionclass ob = new connectionclass();
+	public int BalanceCheck(int accno, int uid)
+    {
+		string str = "select AccBalance from tbAccount where AccNumber=" + accno + " and UserId=" + uid + "";
+		SqlDataReader dr = ob.fnReader(str);
+		int accbal = 0;
+		
+        while (dr.Read())
+        {
+			accbal = Convert.ToInt32(dr["AccBalance"].ToString());
+        }
+		return accbal;
+    }
+	public string GetData(int value)
+	{
+		return string.Format("You entered: {0}", value);
+	}
+
+	public CompositeType GetDataUsingDataContract(CompositeType composite)
+	{
+		if (composite == null)
+		{
+			throw new ArgumentNullException("composite");
+		}
+		if (composite.BoolValue)
+		{
+			composite.StringValue += "Suffix";
+		}
+		return composite;
+	}
+}
---
+++ WCFServicePrjectAsp/App_Code/connectionclass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for connectionclass
+/// </summary>
+public class connectionclass
+{
+    SqlConnection con;
+    SqlCommand cmd;
+    public connectionclass()
+    {
+        con = new SqlConnection(@"server=DESKTOP-P8NIB7B\SQLEXPRESS;database=projectasp;Integrated security=true");
+    }
+    public SqlDataReader fnReader(string sqlquery)
+    {
+        if(con.State==ConnectionState.Open)
+        {
+            con.Close();
+        }
+        cmd = new SqlCommand(sqlquery, con);
+        con.Open();
+        SqlDataReader dr = cmd.ExecuteReader();
+        return dr;
+    }
+}
